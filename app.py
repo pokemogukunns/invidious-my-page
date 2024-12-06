9dea6276b4118b85221fd12c1edaac668d0db62d
pokemogukunns <nattyannkunndesu@icloud.com>
2024-12-07 06:24:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 import subprocess
 import json
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/api', methods=['GET'])
 def get_video_data():
